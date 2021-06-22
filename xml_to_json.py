14dc189bacb0e8a490cebca89cf4bf2d7a050034
Peiwen Su <peiwensu@email.arizona.edu>
2021-06-22 15:28:13 -0700

    Update xml_to_json.py

--- xml_to_json.py
+++ xml_to_json.py
@@ -18,10 +18,10 @@ def parse_element(data):
             if ann.properties[prop_name] is not None:
                 if prop_name == "Type":
                     data_file.append({"value": {"start": start, "end": end, "choices": [ann.properties["Type"]]},
-                                   "id": ann.id, "from_name": ann.type + "-type", "to_name": "text", "type": "choices"})
+                            "id": ann.id, "from_name": ann.type + "-type", "to_name": "text", "type": "choices"})
                 elif prop_name == "Value":
                     data_file.append({"value": {"start": start, "end": end, "text": [ann.properties["Value"]]},
-                                   "id": ann.id, "from_name": ann.type + "-value", "to_name": "text", "type": "textarea"})
+                            "id": ann.id, "from_name": ann.type + "-value", "to_name": "text", "type": "textarea"})
 
                 elif prop_name == "Periods":
                     periods = ann.properties.xml.findall("./Periods")
@@ -31,22 +31,17 @@ def parse_element(data):
                         data_file.append(prop_periods_json)
 
                 elif prop_name.endswith("Interval-Type") or prop_name == "Semantics" or prop_name.endswith("Included"):
-                    data_file.append(type_json(start, end, ann.type, ann.properties, prop_name, ann.id))
+                    data_file.append({"value": {"start": start, "end": end, "choices": [ann.properties[prop_name]]},
+                            "id": ann.id, "from_name": ann.type + "-" + prop_name, "to_name": "text", "type": "choices"})
+
                 else:
-                    data_file.append(relation_json(ann.properties, ann.id, prop_name))
+                    link_entity = ann.properties[prop_name]
+                    if prop_name == "Sub-Interval" and set_to_super_interval:
+                        data_file.append({"from_id": link_entity.id, "to_id": ann.id, "type": "relation", "labels": ["Super-Interval"]})
+                    else:
+                        data_file.append({"from_id": ann.id, "to_id": link_entity.id, "type": "relation", "labels": [prop_name]})
 
     return data_file
-
-
-def relation_json(properties: list, ann_id: str, prop_name: str) -> dict:
-    if prop_name == "Sub-Interval" and set_to_super_interval:
-        return {"from_id": properties[prop_name].id, "to_id": ann_id, "type": "relation", "labels": ["Super-Interval"]}
-    return {"from_id": ann_id, "to_id": properties[prop_name].id, "type": "relation", "labels": [prop_name]}
-
-
-def type_json(start: int, end: int, ann_type: str, properties: list, tag_type_name: str, ann_id: str) -> dict:
-    return {"value": {"start": start, "end": end, "choices": [properties[tag_type_name]]}, "id": ann_id,
-                          "from_name": ann_type + "-" + tag_type_name, "to_name": "text", "type": "choices"}
 
 
 if __name__ == "__main__":
