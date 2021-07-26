bfcc27b0e151b28ac11a15dcea17dbf4e08e269b
Peiwen Su <peiwensu@email.arizona.edu>
2021-07-26 16:48:23 -0700

    Update xml_to_json.py

--- xml_to_json.py
+++ xml_to_json.py
@@ -3,16 +3,21 @@ import anafora
 import sys
 
 
-set_to_super_interval = False
-
-
-def parse_element(data):
+def parse_element(data: anafora.AnaforaData, set_to_super_interval=False):
     data_file = []
     for ann in data.annotations:
-        (start, end), = ann.spans
-        label_json = {"value": {"start": start, "end": end, "labels": [ann.type]}, "id": ann.id, "from_name": "type",
-                      "to_name": "text", "type": "labels"}
-        data_file.append(label_json)
+
+        first_spans = True
+        for (start, end) in ann.spans:
+            if first_spans:
+                label_json = {"value": {"start": start, "end": end, "labels": [ann.type]}, "id": ann.id,
+                              "from_name": "type", "to_name": "text", "type": "labels"}
+                first_spans = False
+            else:
+                label_json = {"value": {"start": start, "end": end, "labels": [ann.type]},
+                              "id": ann.id.split("@")[0] + "_continued" + ann.id.replace(ann.id.split("@")[0], ""),
+                              "from_name": "type", "to_name": "text", "type": "labels"}
+            data_file.append(label_json)
 
         for prop_name in ann.properties:
             if ann.properties[prop_name] is not None:
@@ -23,30 +28,60 @@ def parse_element(data):
                     data_file.append({"value": {"start": start, "end": end, "text": [ann.properties["Value"]]},
                             "id": ann.id, "from_name": ann.type + "-value", "to_name": "text", "type": "textarea"})
 
-                elif prop_name == "Periods":
-                    periods = ann.properties.xml.findall("./Periods")
-                    links = [data.annotations.select_id(period.text) for period in periods]
+                elif prop_name == "Periods" or prop_name == "Repeating-Intervals" \
+                        or prop_name == "Intervals" or prop_name == "Sub-Interval":
+                    duplicate_relations = ann.properties.xml.findall("./" + prop_name)
+                    #links = [data.annotations.select_id(relation.text) for relation in duplicate_relations]
+
+                    links = []
+                    for relation in duplicate_relations:
+                        try:
+                            links.append(data.annotations.select_id(relation.text))
+                        except KeyError:
+                            print(f"entity_id, prop_name :{ann.id, prop_name}")
+
                     for link in links:
-                        prop_periods_json = {"from_id": ann.id, "to_id": link.id, "type": "relation", "labels": ["Periods"]}
-                        data_file.append(prop_periods_json)
+                        data_file.append({"from_id": ann.id, "to_id": link.id, "type": "relation", "labels": [prop_name]})
 
                 elif prop_name.endswith("Interval-Type") or prop_name == "Semantics" or prop_name.endswith("Included"):
                     data_file.append({"value": {"start": start, "end": end, "choices": [ann.properties[prop_name]]},
                             "id": ann.id, "from_name": ann.type + "-" + prop_name, "to_name": "text", "type": "choices"})
 
                 else:
-                    link_entity = ann.properties[prop_name]
-                    if prop_name == "Sub-Interval" and set_to_super_interval:
-                        data_file.append({"from_id": link_entity.id, "to_id": ann.id, "type": "relation", "labels": ["Super-Interval"]})
-                    else:
-                        data_file.append({"from_id": ann.id, "to_id": link_entity.id, "type": "relation", "labels": [prop_name]})
+                    if type(ann.properties[prop_name]) == str:
+                        print(f"entity_id, prop_name :{ann.id, prop_name}")
+                        continue
+                    data_file.append({"from_id": ann.id, "to_id": ann.properties[prop_name].id, "type": "relation", "labels": [prop_name]})
+
+    if set_to_super_interval:
+        data_file = sub_to_super(data_file)
 
     return data_file
+
+
+def sub_to_super(json_list: list):
+    for sub_elem in json_list:
+        if sub_elem["type"] == "relation":
+            if sub_elem["labels"][0] == "Sub-Interval":
+                temp = sub_elem["from_id"]
+                sub_elem["from_id"] = sub_elem["to_id"]
+                sub_elem["to_id"] = temp
+                sub_elem["labels"][0] = "Super-Interval"
+    json_list.sort(key=comp_id)
+    return json_list
+
+
+def comp_id(sub_elem):
+    if sub_elem["type"] != "relation":
+        return int(sub_elem["id"].split("@")[0])
+    elif sub_elem["labels"][0] == "Super-Interval":
+        return int(sub_elem["from_id"].split("@")[0])+0.5
+    else:
+        return int(sub_elem["from_id"].split("@")[0])
 
 
 if __name__ == "__main__":
     with open("data.json", "w") as json_file:
         data = anafora.AnaforaData.from_file(sys.argv[1])
-        #set_to_super_interval = True
         json.dump(list(parse_element(data)), json_file, indent=4)
         json_file.close()
