975b05b12e568b44952c93171eefd8579a69f22c
Peiwen Su <peiwensu@email.arizona.edu>
2021-11-04 17:50:03 -0700

    Create analyze_duplicates.py

---
+++ analyze_duplicates.py
@@ -0,0 +1,55 @@
+import anafora
+import argparse
+import os
+from collections import Counter
+
+
+def main(input_dir, exclude, verbose):
+    duplicate_types = Counter()
+    parent_types = Counter()
+    paths = anafora.walk(input_dir, xml_name_regex=r'TimeNorm\.gold\.completed')
+    for sub_dir, text_file_name, xml_file_names in paths:
+        for xml_file_name in xml_file_names:
+            input_path = os.path.join(input_dir, sub_dir, xml_file_name)
+            data = anafora.AnaforaData.from_file(input_path)
+
+            # find entities that share the same type and span
+            counts = Counter()
+            for entity in data.annotations:
+                #if entity.type not in exclude:
+                counts[entity] += 1
+
+            duplicates = {key for key, count in counts.items() if count > 1}
+
+            # which types are most often duplicated
+            if duplicates:
+                if verbose:
+                    print(f"{xml_file_name}")
+                for entity in sorted(duplicates):
+                    if verbose:
+                        print(f"  {entity.spans} {entity.type}")
+                    duplicate_types[entity.type] += 1
+
+            # which types most often have duplicated entities as arguments
+            for entity in data.annotations:
+                for _, value in entity.properties.items():
+                    if isinstance(value, anafora.AnaforaAnnotation) and value in duplicates:
+                        if verbose:
+                            print(f"  parent: {entity.id} {entity.spans} ----> " 
+                                  f"{value.id} {value.spans}")
+
+                        parent_types[entity.type] += 1
+                        break
+
+    print(f'duplicate types: {duplicate_types}')
+    print(f'parent types:    {parent_types}')
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_dir")
+    parser.add_argument("--exclude", nargs="+")
+    parser.add_argument("--verbose", action='store_true')
+    args = parser.parse_args()
+
+    main(**vars(args))
