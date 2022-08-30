d0e6ddf26a03497d0f39b650aa4d510beab93ffd
Steven Bethard <steven.bethard@gmail.com>
2022-08-29 21:12:45 -0700

    Includes group names from <entities>

--- anafora_to_labelstudio.py
+++ anafora_to_labelstudio.py
@@ -11,8 +11,8 @@ def anafora_schema_to_labelstudio_schema(anafora_path, labelstudio_path):
     ET.SubElement(ls_text_view_elem, 'Text', dict(name="text", value="$text"))
     ls_labels_view_elem = ET.SubElement(ls_view_elem, 'View', dict(
         style="flex: 25%"))
-    ls_labels_elem = ET.SubElement(
-        ls_labels_view_elem, 'Labels', dict(name="type", toName="text"))
+    ls_labels_elem = ET.SubElement(ls_labels_view_elem, 'Labels', dict(
+        name="type", toName="text", showInline="false"))
     ls_relations_elem = ET.SubElement(ls_labels_view_elem, 'Relations')
     relation_types = set()
 
@@ -26,54 +26,57 @@ def anafora_schema_to_labelstudio_schema(anafora_path, labelstudio_path):
     def get(attrib, name):
         return attrib.get(name, default_attributes[name])
 
-    for an_entity_elem in an_root.iter('entity'):
-        entity_type = an_entity_elem.attrib["type"]
-        ET.SubElement(ls_labels_elem, 'Label', dict(value=entity_type))
-        for an_property_elem in an_entity_elem.iter('property'):
-            property_type = an_property_elem.attrib["type"]
-            property_input = an_property_elem.attrib["input"]
-            property_required = get(an_property_elem.attrib, "required")
-            ls_prop_attrib = dict(
-                visibleWhen="region-selected",
-                whenTagName="type",
-                whenLabelValue=entity_type)
+    for an_entities_elem in an_root.iter('entities'):
+        entities_type = an_entities_elem.attrib["type"]
+        ET.SubElement(ls_labels_elem, 'Header', dict(value=f"{entities_type}:"))
+        for an_entity_elem in an_entities_elem.iter('entity'):
+            entity_type = an_entity_elem.attrib["type"]
+            ET.SubElement(ls_labels_elem, 'Label', dict(value=entity_type))
+            for an_property_elem in an_entity_elem.iter('property'):
+                property_type = an_property_elem.attrib["type"]
+                property_input = an_property_elem.attrib["input"]
+                property_required = get(an_property_elem.attrib, "required")
+                ls_prop_attrib = dict(
+                    visibleWhen="region-selected",
+                    whenTagName="type",
+                    whenLabelValue=entity_type)
 
-            if property_input == "text":
-                ls_prop_elem = ET.SubElement(
-                    ls_view_elem, 'View', ls_prop_attrib)
-                ls_text_area_attrib = dict(
-                    name=f"{entity_type}-{property_type}",
-                    perRegion="true")
-                if property_required:
-                    ls_text_area_attrib["required"] = "true"
-                ET.SubElement(ls_prop_elem, 'TextArea', ls_text_area_attrib)
+                if property_input == "text":
+                    ls_prop_elem = ET.SubElement(
+                        ls_view_elem, 'View', ls_prop_attrib)
+                    ls_text_area_attrib = dict(
+                        name=f"{entity_type}-{property_type}",
+                        perRegion="true")
+                    if property_required:
+                        ls_text_area_attrib["required"] = "true"
+                    ET.SubElement(ls_prop_elem, 'TextArea', ls_text_area_attrib)
 
-            elif property_input == "choice":
-                ls_prop_elem = ET.SubElement(
-                    ls_view_elem, 'View', ls_prop_attrib)
-                ls_choices_attrib = dict(
-                    name=f"{entity_type}-{property_type}",
-                    toName="text",
-                    perRegion="true")
-                if property_required:
-                    ls_choices_attrib["required"] = "true"
-                ls_choices_elem = ET.SubElement(
-                    ls_prop_elem, 'Choices', ls_choices_attrib)
-                ET.SubElement(ls_choices_elem, 'Header', dict(
-                    value=f"{property_type}:"))
-                for choice in an_property_elem.text.split(','):
-                    if choice:
-                        ET.SubElement(ls_choices_elem, 'Choice', dict(
-                            value=choice))
+                elif property_input == "choice":
+                    ls_prop_elem = ET.SubElement(
+                        ls_view_elem, 'View', ls_prop_attrib)
+                    ls_choices_attrib = dict(
+                        name=f"{entity_type}-{property_type}",
+                        toName="text",
+                        perRegion="true")
+                    if property_required:
+                        ls_choices_attrib["required"] = "true"
+                    ls_choices_elem = ET.SubElement(
+                        ls_prop_elem, 'Choices', ls_choices_attrib)
+                    ET.SubElement(ls_choices_elem, 'Header', dict(
+                        value=f"{property_type}:"))
+                    for choice in an_property_elem.text.split(','):
+                        if choice:
+                            ET.SubElement(ls_choices_elem, 'Choice', dict(
+                                value=choice))
 
-            elif property_input == "list":
-                if property_type not in relation_types:
-                    ET.SubElement(ls_relations_elem, 'Relation', dict(
-                        value=property_type))
-                    relation_types.add(property_type)
+                elif property_input == "list":
+                    if property_type not in relation_types:
+                        ET.SubElement(ls_relations_elem, 'Relation', dict(
+                            value=property_type))
+                        relation_types.add(property_type)
 
-            else:
-                raise ValueError(f'unexpected input_type: {property_input}')
+                else:
+                    raise ValueError(f'unexpected input_type: {property_input}')
 
     ET.indent(ls_tree, space="  ", level=0)
     ls_tree.write(labelstudio_path)
